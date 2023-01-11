de0a65db2474a1f5bc859dce75fa8609c2feed6c
nosianuri <nurinosia@gmail.com>
2023-01-12 00:46:22 +0600

    add about schema

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -7,6 +7,21 @@ import Footer from '../Shared/Footer';
 import Abanner from './Abanner';
 import Career from './Career';
 
+export const structuredDataSingle = () => {
+  let data = {
+    "@context": "https://schema.org/",
+    "@type": "WebSite",
+    "name": "About Ray Advertising LLC is a Rapidly Growing",
+    "url": "https://rayadvertising.com/about",
+    "potentialAction": {
+      "@type": "SearchAction",
+      "target": "https://rayadvertising.com/service{search_term_string}https://rayadvertising.com/contact",
+      "query-input": "required name=search_term_string"
+    }
+  }
+  return JSON.stringify(data);
+}
+
 const About = () => {
 
   return (
@@ -14,6 +29,9 @@ const About = () => {
       <Helmet>
         <meta charSet="utf-8" />
         <title>About Us | Ray Advertising LLC</title>
+        <script type="application/ld+json">
+          {`${structuredDataSingle()}`}
+        </script>
       </Helmet>
       <Navbar />
       <Abanner />
